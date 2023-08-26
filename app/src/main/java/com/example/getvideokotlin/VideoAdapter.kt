915b6f254664b004dbf47756d3b161dd01e22caa
Ahsan Ahmed <aahsanaahmed26@gmail.com>
2023-08-26 07:46:11 -0700

    Get Videos Kotlin

--- app/src/main/java/com/example/getvideokotlin/VideoAdapter.kt
+++ app/src/main/java/com/example/getvideokotlin/VideoAdapter.kt
@@ -1,5 +1,10 @@
 package com.example.getvideokotlin
 
+import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,12 +36,15 @@ class VideoAdapter(videos: ArrayList<VideoModelClass>) :
         val videoModel: VideoModelClass = video[position]
         holder.title.setText(videoModel.title)
         holder.path.setText(videoModel.path)
+        val thumbnail = getThumbnailFromVideo(videoModel.path.toString(), holder.itemView.context)
 
-        Glide.with(holder.itemView.context)
-            .load(videoModel.thumbnilUri)
-            .placeholder(R.drawable.video_icon)
-            .centerCrop()
-            .into(holder.thumbnil)
+        holder.thumbnil.setImageBitmap(thumbnail)
+
+//        Glide.with(holder.itemView.context)
+//            .load(videoModel.thumbnilUri)
+//            .placeholder(R.drawable.video_icon)
+//            .centerCrop()
+//            .into(holder.thumbnil)
 
     }
 
@@ -51,6 +59,36 @@ class VideoAdapter(videos: ArrayList<VideoModelClass>) :
             path = itemView.findViewById(R.id.file_path)
             thumbnil = itemView.findViewById(R.id.thumbnil)
         }
+    }
+
+    private fun getThumbnailFromVideo(videoPath: String, context: Context): Bitmap {
+        val mediaStoreId = getMediaStoreIdFromPath(videoPath, context.contentResolver)
+        val thumbnail = MediaStore.Video.Thumbnails.getThumbnail(
+            context.contentResolver,
+            mediaStoreId,
+            MediaStore.Video.Thumbnails.MICRO_KIND,
+            null
+        )
+        return thumbnail ?: BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_foreground)
+    }
+
+    private fun getMediaStoreIdFromPath(videoPath: String, contentResolver: ContentResolver): Long {
+        val projection = arrayOf(MediaStore.Video.Media._ID)
+        val selection = "${MediaStore.Video.Media.DATA} = ?"
+        val selectionArgs = arrayOf(videoPath)
+        contentResolver.query(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            null
+        )?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val idColumn = cursor.getColumnIndex(MediaStore.Video.Media._ID)
+                return cursor.getLong(idColumn)
+            }
+        }
+        return -1 // Return a default value if not found
     }
 
 }
